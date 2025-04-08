b4cd709fd20068c1a4f07c0cc3cc94be9d98405d
Buthena AL-Zawaiedh <bouthena762002@gmail.com>
2025-04-08 22:03:59 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,154 @@
+///////////Task1/////////////
+// void main(){
+//   print("Hello, World!");
+// }
+// ////////////Task2/////////////
+// void main(){
+//   String name = "Buthena";
+//   int age = 22;
+
+//   print("My name is $name and I am $age years old.");
+// }
+// ////////////Task3/////////////
+// void main(){
+//   int age = 20;
+
+// if (age >= 18) {
+//     print("You are allowed");
+//   } else {
+//     print("You are not allowed");
+//   }
+// }
+// ////////////Task4/////////////
+// void main(){
+//   List <String> programmingLanguage = ["Dart", "Java", "Python", "C++", "JavaScript"];
+
+//   for (int i = 0; i < programmingLanguage.length; i++) {
+//     print(programmingLanguage[i]);
+//   }
+// }
+// ////////////Task5/////////////
+// void main() {
+//   int a = 5;
+//   int b = 10;
+//   int sum = a + b;
+
+//   print("The Sum is:$sum");
+// }
+///////////////Task6/////////////
+// void main(){
+//   for(int i = 1; i <= 10; i++){
+//     print("5 * $i = ${5 * i}");
+//   }
+// }
+///////////////Task7/////////////
+// void main() {
+//   int a = 9;
+
+//   if (a % 2 == 0) {
+//     print("$a is even number");
+//   } else {
+//     print("$a is odd number");
+//   }
+// }
+////////////////Task8/////////////
+// void main(){
+//   List <int> numbers = [1, 2, 3, 4, 50];
+//   print("The Numbers length is :${numbers.length} and the last number is :${numbers.last}");
+// }
+/////////////////Task9/////////////
+// void main() {
+//   Map<String, dynamic> course = {
+//     'id': 101,
+//     'title': 'Dart Course',
+//     'price': 29
+//   };
+
+//   course.forEach((key, value) {
+//     print('$key: $value');
+//   });
+// }
+/////////////////Task10/////////////
+// void main(){
+//    List<int> number = [1, 2, 2, 3, 4, 4];
+
+//   List<int> uniqueNumber = number.toSet().toList();
+
+//   print("Original: $number , Unique: $uniqueNumber");
+// }
+////////////////Task11/////////////
+// void main() {
+//   List<int> numbers = [1, 2, 3, 4, 5, 6];
+//   List<int> evenNumbers = numbers.where((n) => n % 2 == 0).toList();
+//   print('Even numbers: $evenNumbers');
+// }
+////////////////Task12/////////////
+// void main() {
+//   int numberOfBooks = 5;
+//   double pricePerBook = 12.99;
+
+//   double totalCost = numberOfBooks * pricePerBook;
+//   bool isEven = numberOfBooks % 2 == 0;
+
+//   print('Total cost: \$${totalCost.toStringAsFixed(2)} - Is number of books even? $isEven');
+// }
+// ////////////////Task13/////////////
+// void main() {
+//   double num1 = 12.5;
+//   double num2 = 24.0;
+//   double num3 = 18.5;
+
+//   double average = (num1 + num2 + num3) / 3;
+
+//   print('Average: $average');
+// }
+// ////////////////Task14/////////////
+// void main() {
+//   double length = 10.5;
+//   double width = 5.0;
+
+//   double area = length * width;
+//   double perimeter = 2 * (length + width);
+
+//   print('Area: $area');
+//   print('Perimeter: $perimeter');
+// }
+///////////////Task15/////////////
+// void main() {
+//   double celsius = 25.0;
+
+//   double fahrenheit = (celsius * 9 / 5) + 32;
+
+//   print('Temperature in Fahrenheit: $fahrenheit');
+// }
+// /////////////////Task16/////////////
+// void main() {
+//   int number = 8;
+
+//   String result = (number % 2 == 0) ? 'Even' : 'Odd';
+
+//   print('The number is: $result');
+// }
+// /////////////////Task17///////////// كوتش هذا بالغلط واحد من التاسكات اللي فوق وما انتبهت له
+// void main() {
+//   Map<String, String> user = {
+//     'Name': 'Lina',
+//     'Email': 'lina@example.com',
+//     'City': 'Aqaba',
+//   };
+
+//   user.forEach((key, value) {
+//     print('$key: $value');
+//   });
+// }
+
+
+
+
+
+
+
+
+
+
+
